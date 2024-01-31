f32c3279d0c655a41d1c03cba20346c16894e559
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-01-31 22:25:45 +0530

    scanner closed at end

--- Food_Order.java
+++ Food_Order.java
@@ -11,22 +11,22 @@ public class Food_Order {
         System.out.println("Please Create a Password");
         
         String pass1 = scan.next();
-
+        
         System.out.println("Congratulations Your password Has been Created Successfully");
-
+        
         System.out.println("Your Password is Now: "+pass1);
-
+        
         // System.out.println("Access Granted....!");
-
+        
         System.out.println("\nAre You Hungry yes Or no ?");
         String ans1 = scan.next();
-
+        
         if(ans1.equals("yes")){
             System.out.println("Choose What do you want to between pizza, burger or fish");
             String ans2 = scan.next();
 
             while (!ans2.equals("pizza") && !ans2.equals("burger") && !ans2.equals("fish")){
-
+                
                 System.out.println("Sorry We Do not have it Choose Between pizza,burger or fish.");
                 ans2 = scan.next();
             }
@@ -38,7 +38,7 @@ public class Food_Order {
                     System.out.println("Wrong command Please try again.");
                     ans3= scan.next();
                 }
-
+                
                 if(ans3.equals("yes")){
                     System.out.println("What would you to Drink Coke or Juice ?");
                     String ans4 = scan.next();
@@ -57,20 +57,21 @@ public class Food_Order {
                 }
                 System.out.println("\nFor Verification Please Re-Enter Your Password: ");
                 String pass = scan.next();
-
+                
                 while(!pass.equals(pass1)){
-                System.out.println("This is not the right password please try again: ");
+                    System.out.println("This is not the right password please try again: ");
                 pass = scan.next();
-                }
-                System.out.println("Thank You Your Order is On it's Way..!");
             }
-           else{
-            System.out.println("Alright ! \n Have a Good Day.");
-           }
+            System.out.println("Thank You Your Order is On it's Way..!");
         }
         else{
-            System.out.println("Alright ! \nHave a Good Day.");
+            System.out.println("Alright ! \n Have a Good Day.");
         }
-        
     }
+    else{
+        System.out.println("Alright ! \nHave a Good Day.");
+    }
+    
+    scan.close();   //Scanner Closed
+}
 }
